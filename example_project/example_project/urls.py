d5b6df97f463cc86572ce0bc4b8c2389de05ef4e
Kim Desrosiers <kimdesro@gmail.com>
2014-12-09 13:50:51 -0500

    Update urls.py

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^geoprisma/', include("geoprisma.urls", namespace="geoprisma")),
     url(r'^map/(?P<wsName>[\w-]+)/(?P<viewId>[\w-]+)$', 'example_project.views.maprender'),
 )
